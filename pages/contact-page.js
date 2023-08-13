3af63e8bf3a7cc20515c66c984200beaefbda3b4
tamatsukuri01 <ysk0102tama@gmail.com>
2023-08-13 22:26:57 +0900

    initial commit

---
+++ pages/contact-page.js
@@ -0,0 +1,28 @@
+import Layout from "../components/Layout";
+import Image from "next/image";
+
+const Contact = () => {
+  return (
+    <Layout title="Contact">
+      <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+        <div className="mt-4">
+          <p className="font-bold">Contact info</p>
+        </div>
+        <div className="flex justify-center mt-4">
+          <Image
+            className="rounded-full"
+            src="/avatar.jpg"
+            width={60}
+            height={60}
+            alt="Avatar"
+          />
+        </div>
+        <div className="mt-4">
+          <p className="font-bold">Address</p>
+          <p className="text-xs mt-2 text-gray-600">Tokyo</p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+export default Contact;
